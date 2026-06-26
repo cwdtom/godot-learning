0ecf772c4345e662dff7f2bbab5513bfb02036d2
lingdong <lingdong@bantouyan.com>
2026-06-26 16:34:48 +0800

    Complete player scene with state machine and input setup
    - Add player scene (player.tscn/player.gd) with IDLE/RUN/JUMP/HURT/DEAD states
    - Add main scene, assets (sprites, audio, tilesets, font)
    - Configure input map, physics layer names, nearest texture filter

---
+++ jumgle-jump/player/player.gd
@@ -0,0 +1,95 @@
+extends CharacterBody2D
+
+signal life_changed
+signal died
+
+@export var gravity = 750
+@export var run_speed = 150
+@export var jump_speed = -300
+
+enum {IDLE, RUN, JUMP, HURT, DEAD}
+var state = IDLE
+
+var life = 3: set = set_life
+
+func set_life(value):
+	life = value
+	life_changed.emit(life)
+	if life <= 0:
+		change_state(DEAD)
+		
+func hurt():
+	if state != HURT:
+		change_state(HURT)
+
+func _ready():
+	change_state(IDLE)
+	
+func change_state(new_state):
+	state = new_state
+	match state:
+		IDLE:
+			$AnimationPlayer.play("idle")
+		RUN:
+			$AnimationPlayer.play("run")
+		HURT:
+			$AnimationPlayer.play("hurt")
+			velocity.y = -200
+			velocity.x = -100 * sign(velocity.x)
+			life -= 1
+			await get_tree().create_timer(0.5).timeout
+			change_state(IDLE)
+		JUMP:
+			$AnimationPlayer.play("jump_up")
+		DEAD:
+			died.emit()
+			hide()
+
+func get_input():
+	var right = Input.is_action_pressed("right")
+	var left = Input.is_action_pressed("left")
+	var jump = Input.is_action_just_pressed("jump")
+
+	# movement occurs in all states
+	velocity.x = 0
+	if right:
+		velocity.x += run_speed
+		$Sprite2D.flip_h = false
+	if left:
+		velocity.x -= run_speed
+		$Sprite2D.flip_h = true
+	# only allow jumping when on the ground
+	if jump and is_on_floor():
+		change_state(JUMP)
+		velocity.y = jump_speed
+	# IDLE transitions to RUN when moving
+	if state == IDLE and velocity.x != 0:
+		change_state(RUN)
+	# RUN transitions to IDLE when standing still
+	if state == RUN and velocity.x == 0:
+		change_state(IDLE)
+	# transition to JUMP when in the air
+	if state in [IDLE, RUN] and !is_on_floor():
+		change_state(JUMP)
+	if state == HURT:
+		return
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	get_input()
+
+	move_and_slide()
+	
+	if state == JUMP and is_on_floor():
+		change_state(IDLE)
+	if state == JUMP and velocity.y > 0:
+		$AnimationPlayer.play("jump_down")
+
+func reset(_position):
+	position = _position
+	show()
+	life = 3
+	change_state(IDLE)
